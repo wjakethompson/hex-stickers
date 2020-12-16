21e55052bc5d9599f2e451d19a030ee48493fe3b
Jake Thompson <wjakethompson@gmail.com>
2020-12-16 10:40:56 -0600

    add minorities in r

---
+++ R/scale-logo.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(magick)
+library(glue)
+library(here)
+
+pkg <- "mir"
+
+image_read(glue("~/Desktop/{pkg}.png")) %>%
+  image_trim() %>%
+  image_scale("2521x2911!") %>%
+  image_write(glue("~/Desktop/{pkg}-2.png"))
+  image_write(here("other-stickers", "_png", glue("{pkg}.png")))
+
+
+# image_read(glue("~/Desktop/new-stickers/{pkg}.png")) %>%
+#   image_trim() %>%
+#   image_scale("2206x2557!") %>%
+#   image_write(glue("~/Desktop/{pkg}.png"))
+#
+# image_read(glue("~/Desktop/new-stickers/{pkg}.png")) %>%
+#   image_trim() %>%
+#   # image_scale("2206x2557!") %>%
+#   image_write(glue("~/Desktop/{pkg}2.png"))
+
+
